52016fbf21ea3e8e06a771f321e3ae46db54b11e
aa-r-ju <aarju3447@gmail.com>
2024-12-18 18:24:51 +0545

    feat: completed 02-is-truthy of coersion-and-truthiness

--- workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,1 +1,49 @@
-// YOUR CODE BELOW
+// // YOUR CODE BELOW
+// function isTruthy(para) {
+//   if (para === null) {
+//     return `The null value is falsey`;
+//   } else if (para === false) {
+//     return `The boolean value false is falsey`;
+//   } else if (para === undefined) {
+//     return `undefined is falsey`;
+//   } else if (para === 0) {
+//     return `The number 0 is falsey (the only falsey number)`;
+//   } else if (para === "") {
+//     return `The empty string is falsey (the only falsey string)`;
+//   } else {
+//     return true;
+//   }
+// }
+// console.log(isTruthy("I yearn for truth"));
+// console.log(isTruthy(null));
+// console.log(isTruthy(undefined));
+// console.log(isTruthy(0));
+// console.log(isTruthy(""));
+// console.log(isTruthy(false));
+
+// ANOTHER TECHNIQ
+
+function isTruthy(para) {
+  switch (para) {
+    case null:
+      return `The null value is falsey`;
+    case false:
+      return `The boolean value false is falsey`;
+    case undefined:
+      return `undefined is falsey`;
+    case 0:
+      return `The number 0 is falsey (the only falsey number)`;
+    case "":
+      return `The empty string is falsey (the only falsey string)`;
+
+    default:
+      return true;
+  }
+}
+
+console.log(isTruthy("I yearn for truth"));
+console.log(isTruthy(null));
+console.log(isTruthy(undefined));
+console.log(isTruthy(0));
+console.log(isTruthy(""));
+console.log(isTruthy(false));
