1baa9379660c6a8b3889c8fcc16f4c84150f0809
aa-r-ju <aarju3447@gmail.com>
2024-12-18 18:45:32 +0545

    feat: completed 03-my-or-my-and of coersion-and-truthiness

--- workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
+++ workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
@@ -1,1 +1,24 @@
 // YOUR CODE BELOW
+//USING OPERATORS;
+// function myOr(par1, par2, par3) {
+//   return par1 || par2 || par3;
+// }
+// myOr();
+
+// function myAnd(arg1, arg2, arg3) {
+//   return arg1 && arg2 && arg3;
+// }
+
+//WITHOUT USING OPERATORS;
+
+function myOr(arg1, arg2, arg3) {
+  if (arg1) return arg1;
+  if (arg2) return arg2;
+  return arg3;
+}
+
+function myAnd(arg1, arg2, arg3) {
+  if (!arg1) return arg1;
+  if (!arg2) return arg2;
+  return arg3;
+}
