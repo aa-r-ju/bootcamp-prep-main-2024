62054d000d406fb8267680241d811a17e429d428
aa-r-ju <aarju3447@gmail.com>
2024-12-18 19:51:55 +0545

    feat: completed zero-dark-thirty of coersion and truthiness

--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+function zeroDarkThirty(para) {
+  if (para === 0) {
+    return NaN;
+  }
+
+  let str = String(para);
+  let newval = "";
+  console.log(newval, "kk");
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== "0") {
+      newval += str[i];
+    }
+  }
+
+  return newval === "" ? NaN : Number(newval);
+}
+
+console.log(zeroDarkThirty(102302));
+console.log(zeroDarkThirty(606.203));
