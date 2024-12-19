7f4833432a6c76c2f95b90678f28048a1c9c7715
aa-r-ju <aarju3447@gmail.com>
2024-12-19 13:29:38 +0545

    feat: completed increamental-change of 04 scope.

--- workshops/04-scope/02-incremental-change/incremental-change.js
+++ workshops/04-scope/02-incremental-change/incremental-change.js
@@ -1,3 +1,10 @@
 let counter = 0;
 
 // YOUR CODE BELOW
+function incrementalChange(number) {
+  return (counter += number);
+}
+
+console.log(incrementalChange(2));
+console.log(incrementalChange(6));
+console.log(incrementalChange(-1));
