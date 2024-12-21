cef60c3eb4d28fbc5ced46a399ca17c06fa79962
aa-r-ju <aarju3447@gmail.com>
2024-12-21 21:26:18 +0545

    feat: completed sniper-scope of 04scope

--- workshops/04-scope/04-sniper-scope/sniper-scope.spec.js
+++ workshops/04-scope/04-sniper-scope/sniper-scope.spec.js
@@ -1,57 +1,41 @@
-describe('oddJob', () => {
-
-  it('returns true', () => {
+describe("oddJob", () => {
+  it("returns true", () => {
     let returnedValue = oddJob();
     expect(returnedValue).toEqual(true);
   });
-
 });
 
-
-describe('goldFinger', () => {
-
-  it('returns true', () => {
+describe("goldFinger", () => {
+  it("returns true", () => {
     let returnedValue = goldFinger();
     expect(returnedValue).toEqual(true);
   });
-
 });
 
-
-describe('scaramanga', () => {
-
-  it('returns true', () => {
+describe("scaramanga", () => {
+  it("returns true", () => {
     let returnedValue = scaramanga();
     expect(returnedValue).toEqual(true);
   });
-
 });
 
-
-describe('drNo', () => {
-
-  it('returns true', () => {
+describe("drNo", () => {
+  it("returns true", () => {
     let returnedValue = drNo();
     expect(returnedValue).toEqual(true);
   });
-
 });
 
-
-describe('jaws', () => {
-
-  it('returns true', () => {
+describe("jaws", () => {
+  it("returns true", () => {
     let returnedValue = jaws();
     expect(returnedValue).toEqual(true);
   });
-
 });
 
-describe('elChiffre', () => {
-
-  it('returns true', () => {
+describe("elChiffre", () => {
+  it("returns true", () => {
     let returnedValue = elChiffre();
     expect(returnedValue).toEqual(true);
   });
-
 });
