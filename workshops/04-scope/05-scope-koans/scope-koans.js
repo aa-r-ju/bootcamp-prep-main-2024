324ac207f1bfe0350e790533eeae707cd29054a2
aa-r-ju <aarju3447@gmail.com>
2024-12-21 22:16:58 +0545

    feat: completed scope-knoans of 04 scope

--- workshops/04-scope/05-scope-koans/scope-koans.js
+++ workshops/04-scope/05-scope-koans/scope-koans.js
@@ -1,64 +1,56 @@
 // Test One Restrictions: Do not declare any new variable with the let keyword
-let testOneMessage = 'test failing';
+let testOneMessage = "test failing";
+testOneMessage = "test succeeding";
 
 function testOne() {
   return testOneMessage;
 }
 
-
 // Test Two Restrictions: Do not change any code in the body of testTwo
 // (you can edit helperFunc though)
-let testTwoMessage = 'test failing';
+let testTwoMessage = "test failing";
 
 function testTwo() {
   helperFunc();
   return testTwoMessage;
 }
 
-function helperFunc(a) {
-  a = 'test succeeding';
-  return a;
+function helperFunc() {
+  testTwoMessage = "test succeeding";
+  return testTwoMessage;
 }
 
-
 // Test Three Restrictions: Type only a single character in either testThree or getMessage
-let testThreeMessage = 'test failing';
+let testThreeMessage = "test failing";
 
 function testThree(testThreeMessage) {
   if (testThreeMessage) {
-    testThreeMessage = 'test succeeding';
+    testThreeMessage = "test succeeding";
   }
 
   let msg = getMessage();
   return msg;
 
   function getMessage(value) {
-    if (value) {
-      return testThreeMessage;
-    }
-
-    return 'test failing';
+    return testThreeMessage;
   }
 }
 
-
 // Test Four Restrictions: Delete only a single character from any function.
-let testFourMessage = 'test succeeding';
+let testFourMessage = "test succeeding";
 
 function testFour(msg) {
   function innerFunc(msg) {
-    msg = msg
-
     function doubleInner(msg) {
       testFourMessage = msg;
       return testFourMessage;
     }
 
-    testFourMessage = doubleInner('test failing')
+    testFourMessage = doubleInner("test failing");
   }
 
   innerFunc(testFourMessage);
 
-  msg = testFourMessage;
+  testFourMessage = "test succeeding";
   return testFourMessage;
 }
