747ba1bbab46e1b903af71d9d4bb9500f92cb83b
aa-r-ju <aarju3447@gmail.com>
2024-12-22 13:31:32 +0545

    feat: completed odd-couple of 05 array-i

--- workshops/05-arrays-i/01-odd-couple/odd-couple.js
+++ workshops/05-arrays-i/01-odd-couple/odd-couple.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function oddCouple(arr) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      newArr.push(arr[i]);
+    }
+    if (newArr.length === 2) {
+      return newArr;
+    }
+  }
+  return newArr;
+}
+
+console.log(oddCouple([1, 2, 3, 4, 5]));
