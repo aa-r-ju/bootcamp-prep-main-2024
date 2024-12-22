d1fd9a600de9e44a6ee18e0705590a7217cb6590
aa-r-ju <aarju3447@gmail.com>
2024-12-22 20:38:56 +0545

    feat: completed remove-columns of 06-array-ii

--- workshops/06-arrays-ii/05-remove-columns/remove-columns.js
+++ workshops/06-arrays-ii/05-remove-columns/remove-columns.js
@@ -1,1 +1,23 @@
 // YOUR CODE BELOW
+function removeColumns(originalGrid, numColumns) {
+  let newGrid = [];
+
+  for (let i = 0; i < originalGrid.length; i++) {
+    let newRow = originalGrid[i].slice(0, originalGrid[i].length - numColumns);
+    newGrid.push(newRow);
+  }
+
+  return newGrid;
+}
+
+console.log(
+  removeColumns(
+    [
+      [1, 2, 3],
+      [1, 2, 3],
+      [1, 2, 3],
+      [1, 2, 3],
+    ],
+    2
+  )
+);
