9556f03770f37a63b52bc74dda10254cdaf9b5dc
aa-r-ju <aarju3447@gmail.com>
2024-12-22 14:34:15 +0545

    feat: completed my-reverse of 05 array-i

--- workshops/05-arrays-i/04-my-reverse/my-reverse.js
+++ workshops/05-arrays-i/04-my-reverse/my-reverse.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function myReverse(arr) {
+  let startIndex = arr.length - 1;
+  let newArr = [];
+
+  for (let i = startIndex; i >= 0; i--) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
+}
+console.log(myReverse([1, 2, 3]));
