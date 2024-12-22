6a9ecabfe69ba663635509e6678c249ed860fe70
aa-r-ju <aarju3447@gmail.com>
2024-12-22 20:22:23 +0545

    feat: completed make-grid of 06 array-ii

--- workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,24 @@
-// YOUR CODE BELOW
+// // YOUR CODE BELOW
+function makeGrid(numColumns, numRows) {
+  let grid = [];
+
+  for (let i = 0; i < numRows; i++) {
+    // console.log(i);
+    let row = [];
+    for (let j = 1; j <= numColumns; j++) {
+      //   console.log(j);
+      row.push(j);
+      console.log(row);
+    }
+    grid.push(row);
+  }
+
+  return grid;
+}
+
+console.log(makeGrid(3, 4));
+/* => [[1, 2, 3],
+         [1, 2, 3],
+         [1, 2, 3],
+         [1, 2, 3]]
+  */
