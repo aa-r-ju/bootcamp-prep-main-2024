96dc4fcf99fbf9f796bd89c8916ff52a534dcaa1
aa-r-ju <aarju3447@gmail.com>
2024-12-22 21:04:43 +0545

    feat: completed my-join of 06-array-ii

--- workshops/06-arrays-ii/06-my-join/my-join.js
+++ workshops/06-arrays-ii/06-my-join/my-join.js
@@ -1,1 +1,20 @@
-// YOUR CODE BELOW
+// // YOUR CODE BELOW
+function myJoin(array, separator = ",") {
+  let str = "";
+  for (let i = 0; i < array.length; i++) {
+    let element = array[i];
+    if (element === undefined || element === null) {
+      element = "";
+    }
+    str += element;
+
+    if (i < array.length - 1) {
+      str += separator;
+    }
+  }
+
+  return str;
+}
+console.log(myJoin(["let's", "make", "a", "list"], " "));
+console.log(myJoin(["Peter", "Paul", "Mary"]));
+console.log(myJoin(["hello", undefined, "world"], "-"));
