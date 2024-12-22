d0e7ee93d4bb3ffcb26c85deae2885de9b6db325
aa-r-ju <aarju3447@gmail.com>
2024-12-22 19:54:17 +0545

    feat: completed zoo-inventory of 06-array-ii

--- workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
+++ workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
@@ -1,1 +1,22 @@
-// YOUR CODE BELOW
+// // YOUR CODE BELOW
+function zooInventory(zoo) {
+  let all = [];
+  for (let i = 0; i < zoo.length; i++) {
+    let animalName = zoo[i][0];
+    let animalDetails = zoo[i][1];
+    let animalType = animalDetails[0];
+    let animalAge = animalDetails[1];
+
+    let sentence = `${animalName} the ${animalType} is ${animalAge}.`;
+    all.push(sentence);
+  }
+  console.log(all);
+  return all;
+}
+let myZoo = [
+  ["King Kong", ["gorilla", 42]],
+  ["Nemo", ["fish", 5]],
+  ["Punxsutawney Phil", ["groundhog", 11]],
+];
+
+zooInventory(myZoo);
