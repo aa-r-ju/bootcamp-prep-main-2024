16ba1ba50addb795010a9c80c8634248c40ec907
aa-r-ju <aarju3447@gmail.com>
2024-12-22 15:21:07 +0545

    feat: completed even-and-odd of 06 array-ii

--- workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
+++ workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+function evenAndOdd(arr) {
+  let bigArr = [];
+  let even = [];
+  let odd = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      even.push(arr[i]);
+    } else {
+      odd.push(arr[i]);
+    }
+  }
+  bigArr.push(even);
+  bigArr.push(odd);
+  return bigArr;
+}
+console.log(evenAndOdd([1, 2, 3, 4, 5, 6]));
