a8cf501da7f0f6d6311bcd442d7412bec3d199ec
aa-r-ju <aarju3447@gmail.com>
2024-12-23 22:42:22 +0545

    feat: completed rotate-array of 06-array-ii

--- workshops/06-arrays-ii/08-rotate-array/rotate-array.js
+++ workshops/06-arrays-ii/08-rotate-array/rotate-array.js
@@ -1,1 +1,26 @@
 // YOUR CODE BELOW
+function rotateArray(originalArray, rotateNum) {
+  let arr = [];
+
+  if (rotateNum === 0) {
+    return [...originalArray];
+  }
+
+  if (rotateNum > 0) {
+    let cutIndex = originalArray.length - rotateNum;
+    arr = originalArray
+      .slice(cutIndex)
+      .concat(originalArray.slice(0, cutIndex));
+  } else {
+    let cutIndex = -rotateNum;
+    arr = originalArray
+      .slice(cutIndex)
+      .concat(originalArray.slice(0, cutIndex));
+  }
+
+  return arr;
+}
+
+console.log(rotateArray([1, 2, 3, 4, 5], 1));
+console.log(rotateArray([1, 2, 3, 4, 5], -1));
+console.log(rotateArray([1, 2, 3, 4, 5], 0));
