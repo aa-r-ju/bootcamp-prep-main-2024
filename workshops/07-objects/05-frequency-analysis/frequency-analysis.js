83589b83c872ab970b60e967fcd63bf2426cd2e9
aa-r-ju <aarju3447@gmail.com>
2024-12-26 19:59:04 +0545

    feat: completed frequency-analysis of 07-object

--- workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function frequencyAnalysis(alpha) {
+  let obj = {};
+  for (let val of alpha) {
+    if (obj[val]) {
+      obj[val]++;
+    } else {
+      obj[val] = 1;
+    }
+  }
+  return obj;
+}
+
+console.log(frequencyAnalysis("abacdefagaaaaaaaa"));
