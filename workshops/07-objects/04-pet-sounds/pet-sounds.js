8de500ffd148da1f1830a407093c912c4a74c837
aa-r-ju <aarju3447@gmail.com>
2024-12-26 19:42:20 +0545

    feat: completed pet-sound of 07-object

--- workshops/07-objects/04-pet-sounds/pet-sounds.js
+++ workshops/07-objects/04-pet-sounds/pet-sounds.js
@@ -1,40 +1,57 @@
 let animalNoises = [
-  { 'dog': {
-    'America' : 'Woof! Woof!',
-    'Germany' : 'Wau Wau!',
-    'England' : 'Bow wow!',
-    'Uruguay' : 'Jua jua!',
-    'Afrikaans' : 'Blaf!',
-    'Korea' : 'Mong mong!',
-    'Iceland' : 'Voff voff!',
-    'Albania' : 'Ham!',
-    'Algeria' : 'Ouaf ouaf!'
-    }
+  {
+    dog: {
+      America: "Woof! Woof!",
+      Germany: "Wau Wau!",
+      England: "Bow wow!",
+      Uruguay: "Jua jua!",
+      Afrikaans: "Blaf!",
+      Korea: "Mong mong!",
+      Iceland: "Voff voff!",
+      Albania: "Ham!",
+      Algeria: "Ouaf ouaf!",
+    },
   },
-  { 'cat': {
-    'America' : 'Meow',
-    'Germany' : 'Miauw!',
-    'England' : 'mew mew',
-    'Uruguay' : 'Miau Miau!',
-    'Afrikaans' : 'Purr',
-    'Korea' : 'Nyaong!',
-    'Iceland' : 'Kurnau!',
-    'Albania' : 'Miau',
-    'Algeria' : 'Miaou!'
-    }
+  {
+    cat: {
+      America: "Meow",
+      Germany: "Miauw!",
+      England: "mew mew",
+      Uruguay: "Miau Miau!",
+      Afrikaans: "Purr",
+      Korea: "Nyaong!",
+      Iceland: "Kurnau!",
+      Albania: "Miau",
+      Algeria: "Miaou!",
+    },
   },
-  { 'chicken': {
-    'America' : 'Cluck cluck',
-    'Germany' : 'tock tock tock',
-    'England' : 'Cluck Cluck',
-    'Uruguay' : 'gut gut gdak',
-    'Afrikaans' : 'kukeleku',
-    'Korea' : 'ko-ko-ko',
-    'Iceland' : 'Chuck-chuck!',
-    'Albania' : 'Kotkot',
-    'Algeria' : 'Cotcotcodet'
-    }
-  }
+  {
+    chicken: {
+      America: "Cluck cluck",
+      Germany: "tock tock tock",
+      England: "Cluck Cluck",
+      Uruguay: "gut gut gdak",
+      Afrikaans: "kukeleku",
+      Korea: "ko-ko-ko",
+      Iceland: "Chuck-chuck!",
+      Albania: "Kotkot",
+      Algeria: "Cotcotcodet",
+    },
+  },
 ];
 
 // YOUR CODE BELOW
+function petSounds(animal, country) {
+  for (let entry of animalNoises) {
+    if (entry[animal]) {
+      let sound = entry[animal][country];
+      if (sound) {
+        return `${
+          animal[0].toUpperCase() + animal.slice(1)
+        }s in ${country} say ${sound}`;
+      }
+    }
+  }
+  return `No information available for ${animal}s in ${country}.`;
+}
+console.log(petSounds("cat", "Iceland"));
