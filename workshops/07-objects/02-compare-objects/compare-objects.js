481d73ac396d57688e3d63c2b32258081928089b
aa-r-ju <aarju3447@gmail.com>
2024-12-26 17:28:10 +0545

    feat: completed compare-objects of 07- objects

--- workshops/07-objects/02-compare-objects/compare-objects.js
+++ workshops/07-objects/02-compare-objects/compare-objects.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+
+function compareObjects(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  console.log(keys1, "test");
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// console.log(compareObjects({ name: "nick" }, { name: "nick" }));
+// console.log(compareObjects({ name: "zeke" }, { name: "zeke", age: 12 }));
