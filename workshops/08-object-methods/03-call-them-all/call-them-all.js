fdb0d9c2756553bc145476273da9dacc08887596
aa-r-ju <aarju3447@gmail.com>
2024-12-29 20:22:16 +0545

    feat: completed call-them-all of 08-objects-method

--- workshops/08-object-methods/03-call-them-all/call-them-all.js
+++ workshops/08-object-methods/03-call-them-all/call-them-all.js
@@ -1,1 +1,26 @@
 // YOUR CODE BELOW
+
+let addsNums = {
+  addTen: function (num) {
+    return num + 10;
+  },
+
+  addTwenty: function (num) {
+    return num + 20;
+  },
+
+  someProperty: "value",
+};
+
+function callThemAll(obj, value) {
+  let result = [];
+
+  for (let key in obj) {
+    if (typeof obj[key] === "function") {
+      result.push(obj[key](value));
+    }
+  }
+  return result;
+}
+
+console.log(callThemAll(addsNums, 100));
