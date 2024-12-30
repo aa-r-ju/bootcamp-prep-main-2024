4247d7c3345856aa21e1467b10348df3f9e30143
aa-r-ju <aarju3447@gmail.com>
2024-12-30 16:14:27 +0545

    feat: completed clone-machine of 09-PBV_PBR

--- workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
+++ workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+function cloneMachine(animal) {
+  let clone = {
+    name: animal.name + "Clone",
+    species: animal.species,
+    offspring: [],
+  };
+
+  animal.offspring.push(clone.name);
+
+  return clone;
+}
+
+let dolly = {
+  name: "Dolly",
+  species: "sheep",
+  offspring: [],
+};
+let dollyClone = cloneMachine(dolly);
+console.log(dollyClone);
+console.log(dolly);
