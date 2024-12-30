c0c86306f1e3893097122531ce3b0457d8dae723
aa-r-ju <aarju3447@gmail.com>
2024-12-30 15:52:13 +0545

    feat: completed very-odd-mutant of 09-PBV_PBR

--- workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
+++ workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function veryOddMutant(nums) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+      nums[i] = "normie";
+      count++;
+    }
+  }
+  return count;
+}
