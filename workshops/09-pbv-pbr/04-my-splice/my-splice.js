a02e6b737ddaa89bdbaa99f15bb6237aa9280c59
aa-r-ju <aarju3447@gmail.com>
2024-12-31 17:11:40 +0545

    fix: deleted commented code

--- workshops/09-pbv-pbr/04-my-splice/my-splice.js
+++ workshops/09-pbv-pbr/04-my-splice/my-splice.js
@@ -1,36 +1,4 @@
 // YOUR CODE BELOW
-// function mySplice(arr, start, deleteCount, newElement) {
-//   let removedElements = [];
-
-//   for (let i = start; i < start + deleteCount; i++) {
-//     removedElements.push(arr[i]);
-//   }
-
-//   let resultArray = [];
-//   for (let i = 0; i < start; i++) {
-//     resultArray.push(arr[i]);
-//   }
-//   if (newElement !== undefined) {
-//     resultArray.push(newElement);
-//   }
-//   for (let i = start + deleteCount; i < arr.length; i++) {
-//     resultArray.push(arr[i]);
-//   }
-
-//   for (let i = 0; i < resultArray.length; i++) {
-//     arr[i] = resultArray[i];
-//   }
-//   arr.length = resultArray.length;
-
-//   return removedElements;
-// }
-
-// let myArray = [1, 2, 3];
-// let removedItems = mySplice(myArray, 1, 1, "apples");
-
-// console.log(removedItems); // [2]
-// console.log(myArray); // [1, 'apples', 3]
-
 function mySplice(arr, start, end, value) {
   let reverseArray = [];
 
