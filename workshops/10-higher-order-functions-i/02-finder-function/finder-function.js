6f54f850247c5c783a6cb4ca24dd5a0d321f08a8
aa-r-ju <aarju3447@gmail.com>
2025-01-01 19:36:24 +0545

    feat: completed finder-function of 10-Higher-order-function

--- workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
+++ workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+function finderFunction(arr, callbacks) {
+  for (let i = 0; i < arr.length; i++) {
+    if (callbacks(arr[i])) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+let numbers = [1, 3, 5, 64, 7, 12];
+let odds = [9, 13, 15, 17];
+
+function isEven(num) {
+  return !(num % 2);
+}
+
+console.log(finderFunction(numbers, isEven)); // 3
+console.log(finderFunction(odds, isEven)); // -1
