e010511094adcb1fe4eb3570c5b85d5bba2a814f
aa-r-ju <aarju3447@gmail.com>
2025-01-02 17:21:12 +0545

    feat: completed cache-saving of 11-higher-order-function-ii

--- workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
+++ workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function cacheSavings(callback) {
+  const cache = {};
+
+  return function (arg) {
+    if (cache.hasOwnProperty(arg)) {
+      console.log("Returning from cache...");
+      return cache[arg];
+    } else {
+      console.log("Calculating result...");
+      const result = callback(arg);
+      cache[arg] = result;
+      return result;
+    }
+  };
+}
