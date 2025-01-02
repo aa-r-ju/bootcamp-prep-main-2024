4abed062d4ef38d66d85e49e091fb0bb6a92805d
aa-r-ju <aarju3447@gmail.com>
2025-01-02 15:42:26 +0545

    feat: completed biller-builder of 11-higher-function-ii

--- workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
+++ workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+function billerBuilder(state) {
+  return function billi(price) {
+    let paisa = price;
+    if (state === "NY") {
+      paisa += paisa * 0.03;
+      paisa += paisa * 0.04;
+    } else if (state === "NJ") {
+      paisa += paisa * 0.05;
+      paisa += paisa * 0.06625;
+    }
+
+    return paisa;
+  };
+}
+
+let newYorkBiller = billerBuilder("NY");
+console.log(newYorkBiller(100));
+let newJersBiller = billerBuilder("NJ");
+console.log(newJersBiller(100));
