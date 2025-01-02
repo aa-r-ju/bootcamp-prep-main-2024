372045df8fdd2b4ff8fa7dc356d29a4a430143ec
aa-r-ju <aarju3447@gmail.com>
2025-01-02 21:46:41 +0545

    feat: completed reverse-array of 12-recursion

--- workshops/12-recursion-i/05-reverse-array/reverse-array.js
+++ workshops/12-recursion-i/05-reverse-array/reverse-array.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function reverseArray(arr) {
+  if (arr.length === 0) return [];
+  return [arr[arr.length - 1]].concat(reverseArray(arr.slice(0, -1)));
+}
+
+let arr = [1, 2, 3, 4];
+let reversedArr = reverseArray(arr);
+
+console.log(reversedArr); // [4,3,2,1]
+console.log(arr); // [1,2,3,4]
