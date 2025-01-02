b86ff5e3b4dc8deb82a7a921175f52addeb808a1
aa-r-ju <aarju3447@gmail.com>
2025-01-02 15:47:56 +0545

    feat: completed times-table of 11-higher-order-function-ii

--- workshops/11-higher-order-functions-ii/02-times-table/times-table.js
+++ workshops/11-higher-order-functions-ii/02-times-table/times-table.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+function timesTable(num) {
+  return function (num1) {
+    let total = 0;
+    total += num * num1;
+    return total;
+  };
+}
+
+let ninesTable = timesTable(9);
+console.log(ninesTable(8));
+let twelvesTable = timesTable(12);
+console.log(twelvesTable(100));
