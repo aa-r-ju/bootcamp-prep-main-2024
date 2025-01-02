5c3b02c7620afde039fd2fa7a6641e1b720b1a75
aa-r-ju <aarju3447@gmail.com>
2025-01-02 21:55:07 +0545

    feat: finished is-palindrome of 12-recursion

--- workshops/12-recursion-i/07-is-palindrome/is-palindrome.js
+++ workshops/12-recursion-i/07-is-palindrome/is-palindrome.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function isPalindrome(str) {
+  str = str.toLowerCase();
+  if (str.length <= 1) return true;
+  if (str[0] !== str[str.length - 1]) return false;
+  return isPalindrome(str.slice(1, str.length - 1));
+}
+
+console.log(isPalindrome("Kayak")); // Output: true
+console.log(isPalindrome("NEVERODDOREVEN")); // Output: true
+console.log(isPalindrome("Tacocat")); // Output: true
+console.log(isPalindrome("SELFLESS")); // Output: false
