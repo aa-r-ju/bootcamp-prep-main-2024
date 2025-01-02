d3e6de1f65847c2b73f472bd31273339eb052f53
aa-r-ju <aarju3447@gmail.com>
2025-01-02 21:40:48 +0545

    feat: completed count-vowels of 12-recursion

--- workshops/12-recursion-i/04-count-vowels/count-vowels.js
+++ workshops/12-recursion-i/04-count-vowels/count-vowels.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function countVowels(str) {
+  if (str === "") return 0;
+  const vowels = "aeiouAEIOU";
+  const currentChar = str[0];
+
+  const isVowel = vowels.includes(currentChar) ? 1 : 0;
+
+  return isVowel + countVowels(str.slice(1));
+}
+
+countVowels("Four score and seven years"); // => 9
