000fdfe0e0f83c8d08546ac30cafc79669d02b85
aa-r-ju <aarju3447@gmail.com>
2025-01-02 17:01:43 +0545

    feat: finished get-dog-breeder of 11-higher-order-function-ii

--- workshops/11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js
+++ workshops/11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js
@@ -1,1 +1,30 @@
-// YOUR CODE BELOW
+// // YOUR CODE BELOW
+function getDogBreeder(defaultName, defaultAge) {
+  return function (name, age) {
+    if (typeof name === "number") {
+      age = name;
+      name = defaultName;
+    }
+
+    if (typeof name === "undefined") {
+      name = defaultName;
+    }
+
+    if (typeof age === "undefined") {
+      age = defaultAge;
+    }
+
+    return { name, age };
+  };
+}
+
+let puppyFarm = getDogBreeder("Snoopy", 0);
+let rescueShelter = getDogBreeder("Odie", 3);
+
+console.log(puppyFarm("Olaf", 3));
+
+console.log(puppyFarm(2));
+
+console.log(rescueShelter());
+
+console.log(rescueShelter(10));
