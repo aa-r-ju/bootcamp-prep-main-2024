b9f39378f1222cae0fbae0b3afd2c40f5e64f9c0
aa-r-ju <aarju3447@gmail.com>
2025-01-02 16:00:01 +0545

    feat: finished partial of 11-higher-order-function-ii

--- workshops/11-higher-order-functions-ii/03-partial/partial.js
+++ workshops/11-higher-order-functions-ii/03-partial/partial.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+function partial(callback, arg) {
+  return function (arg1) {
+    return callback(arg, arg1);
+  };
+}
+
+function summer(a, b) {
+  return a + b;
+}
+
+let sumFive = partial(summer, 5);
+console.log(sumFive(10));
