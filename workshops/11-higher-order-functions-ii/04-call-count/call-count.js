43b58bc7e984edb844a7bb938c514a8b19e5a396
aa-r-ju <aarju3447@gmail.com>
2025-01-02 16:09:14 +0545

    feat: finished call-count of 11-higher-order-function-ii

--- workshops/11-higher-order-functions-ii/04-call-count/call-count.js
+++ workshops/11-higher-order-functions-ii/04-call-count/call-count.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+function callCount() {
+  let count = 0;
+  return function maker() {
+    count++;
+    return count;
+  };
+}
+
+let newFunction1 = callCount();
+let newFunction2 = callCount();
+
+console.log(newFunction1());
+console.log(newFunction1());
+
+console.log(newFunction2());
+console.log(newFunction2());
+console.log(newFunction2());
