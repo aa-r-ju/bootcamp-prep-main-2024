22c38f4c9f32dcb5d0d0aa924ea503ef674d976e
aa-r-ju <aarju3447@gmail.com>
2025-01-03 21:39:10 +0545

    feat: completed array-sum of 13-recursion-ii

--- workshops/13-recursion-ii/01-array-sum/array-sum.js
+++ workshops/13-recursion-ii/01-array-sum/array-sum.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function arraySum(arr) {
+  let sum = 0;
+
+  for (let element of arr) {
+    if (Array.isArray(element)) {
+      sum += arraySum(element);
+    } else {
+      sum += element;
+    }
+  }
+
+  return sum;
+}
+
+console.log(arraySum([1, [2, 3, [4]]]));
